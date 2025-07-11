626195d211e795c107b45fee3f392b6bf03e28dc
Kirucoderanger <klemu@byupathway.edu>
2025-07-12 01:59:36 +0300

    W02 Individual Activity: Dynamic Product List

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,38 @@
+import { renderListWithTemplate } from "./utils.mjs";
+//import {getData} from "./ProductData.mjs";
+
+function productCardTemplate(product) {
+  return `
+    <li class="product-card">
+      <a href="product_pages/?product=${product.Id}">
+        <img src="${product.Image}" alt="${product.Name}">
+        <h2>${product.Brand.Name}</h2>
+        <h3>${product.Name}</h3>
+        <p class="product-card__price">$${product.FinalPrice}</p>
+      </a>
+    </li>
+    `;
+}
+//const listElement = document.querySelector(".products");
+export default class ProductList {
+  constructor(category, dataSource, listElement) {
+    this.category = category;
+    this.dataSource = dataSource;
+    this.listElement = listElement;
+  }
+
+  async init() {
+    const list = await this.dataSource.getData();
+    this.renderList(list);
+  }
+
+  renderList(list) {
+    
+    
+    renderListWithTemplate(productCardTemplate, this.listElement, list,);
+    
+
+  }
+
+}
+
