02f6d7dbbc96f030a91c7ce93dd4c768b053fb7f
Kirucoderanger <klemu@byupathway.edu>
2025-07-13 02:38:37 +0300

    Fix publicDir path for Vite and remove manual copy in build script

--- vite.config.js
+++ vite.config.js
@@ -34,7 +34,7 @@ export default defineConfig({
   },
 });*/
 
-
+/*
 import { defineConfig } from "vite";
 import { resolve } from "path";
 
@@ -54,4 +54,23 @@ export default defineConfig({
     assetsInlineLimit: 0, // make sure no asset is inlined (optional)
   },
   publicDir: "public", // <-- make sure this is set to your public folder
+});*/
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "src",
+  publicDir: "../public",  // <-- This tells Vite where your public folder is, relative to 'root'
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        cart: resolve(__dirname, "src/cart/index.html"),
+        checkout: resolve(__dirname, "src/checkout/index.html"),
+        product: resolve(__dirname, "src/product_pages/index.html"),
+      },
+    },
+  },
 });
+
