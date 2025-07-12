f8ac8f9431a7d3930a679afcb2c309d725c2f902
Kirucoderanger <klemu@byupathway.edu>
2025-07-13 01:04:14 +0300

    absolute path corrected in order to Vite maps this correctly in both dev and prod builds

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,26 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 
-export default defineConfig({
+/*export default defineConfig({
   root: "src/",
 
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        cart: resolve(__dirname, "src/cart/index.html"),
+        checkout: resolve(__dirname, "src/checkout/index.html"),
+        product: resolve(__dirname, "src/product_pages/index.html"),
+      },
+    },
+  },
+});
+*/
+
+export default defineConfig({
+  root: "src/",
+  base: "./", // <-- add this line
   build: {
     outDir: "../dist",
     rollupOptions: {
