6dc419845f756aa86ff07d55c18d06371265e801
Kirucoderanger <klemu@byupathway.edu>
2025-07-13 20:24:28 +0300

    Fix: deploy publicDir json to Netlify using vite.config and netlify.toml

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,26 @@
-import { resolve } from "path";
+/*import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "src/",
+
+  build: {
+    outDir: "../dist",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        cart: resolve(__dirname, "src/cart/index.html"),
+        checkout: resolve(__dirname, "src/checkout/index.html"),
+        product: resolve(__dirname, "src/product_pages/index.html"),
+      },
+    },
+  },
+});*/
+import { resolve } from "path";
+import { defineConfig } from "vite";
+export default defineConfig({
+  root: "src/",
+  publicDir: "../public", // Tells Vite to copy assets from public → dist
 
   build: {
     outDir: "../dist",
