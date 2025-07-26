77697b0b56eaa501fccc65c5ba5d75802caf089b
Kirucoderanger <klemu@byupathway.edu>
2025-07-26 20:54:24 +0300

    resole the success page in to vite config

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,8 @@ export default defineConfig({
         checkout: resolve(__dirname, "src/checkout/index.html"),
         product: resolve(__dirname, "src/product_pages/index.html"),
         product_listing: resolve(__dirname, "src/product_listing/index.html"),
+        //Add success page
+        success: resolve(__dirname, "src/checkout/success.html"),
       },
     },
   },
