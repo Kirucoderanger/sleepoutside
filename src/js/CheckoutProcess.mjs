f02f6c453d6aa28c7e069b4d3dc2b3d1245c86e7
Kirucoderanger <klemu@byupathway.edu>
2025-07-26 18:22:27 +0300

    success page path for netfly

--- src/js/CheckoutProcess.mjs
+++ src/js/CheckoutProcess.mjs
@@ -96,7 +96,7 @@ export default class CheckoutProcess {
       const response = await services.checkout(order);
       console.log(response);
       setLocalStorage("so-cart", []);
-      location.assign("/checkout/success.html");
+      location.assign("../checkout/success.html");
     } catch (err) {
         removeAllAlerts();
         //for (let message in err.message) {
