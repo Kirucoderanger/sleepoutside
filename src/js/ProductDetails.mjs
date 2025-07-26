f9a07bcfcf5f400b64725adc7635522c46e39f68
Kirucoderanger <klemu@byupathway.edu>
2025-07-26 18:18:41 +0300

    alert for adding cart item

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,4 +1,4 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage, alertMessage } from "./utils.mjs";
 
 export default class ProductDetails {
 
@@ -26,6 +26,7 @@ export default class ProductDetails {
     const cartItems = getLocalStorage("so-cart") || [];
     cartItems.push(this.product);
     setLocalStorage("so-cart", cartItems);
+    alertMessage(`${this.product.NameWithoutBrand} added to cart!`);
   }
 
   renderProductDetails() {
